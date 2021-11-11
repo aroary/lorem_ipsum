a062cf282ec80a4843dcd5189c76a26366b02c14
aroary <meanchemshkedi@gmail.com>
2021-11-11 15:00:09 -0500

    cleanup

--- utilities/lorem_ipsum.js
+++ utilities/lorem_ipsum.js
@@ -7,25 +7,23 @@ const generate = (n) => {
 
     for (let i = 0; i < n; i++) {
         // Start every generated string with 'lorem ipsum dolor sit amet consectetur adipisicing elit.'
-        if (i < 9)
-            generated.push('lorem ipsum dolor sit amet consectetur adipisicing elit.'.split(' ')[i]);
+        if (i < 9) generated.push('lorem ipsum dolor sit amet consectetur adipisicing elit.'.split(' ')[i]);
         else generated.push(randomItem(words) + randomItem(["", "", "", "", ".", ",", "!", "?"]));
-    }
+    };
 
-    let fullString = generated.join(' ').replace(/  /gm, ' ');
-    const splitted = fullString.split(' ');
+    let fullString = generated.join` `.replace(/  /gm, ' ');
+    const splitted = fullString.split` `;
     for (let i = 0; i < splitted.length; i++) {
         if ([".", "!", "?"].includes(splitted[i].slice(-1))) {
-            const charPos = splitted.slice(0, i + 1).join(' ').length + 1;
+            const charPos = splitted.slice(0, i + 1).join` `.length + 1;
             fullString = 
                 fullString.slice(0, charPos) + 
                 fullString.charAt(charPos).toUpperCase() + 
                 fullString.slice(charPos, fullString.length);
-        }
-    }
+        };
+    };
 
-    [/,\./, /,!/, /,\?/, /!!/, /\.\./, /\?\?/, / \?/, / \./, / \!/]
-        .forEach(r => fullString.replace(r, ''));
+    [/,\./, /,!/, /,\?/, /!!/, /\.\./, /\?\?/, / \?/, / \./, / \!/].forEach(r => fullString.replace(r, ''));
 
     return fullString.charAt(0).toUpperCase() + fullString.slice(1, fullString.length) + ".";
 };
