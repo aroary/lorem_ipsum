7d6e82f7ec58739872f44d14c9ba66c775e8e6e0
Atticus C <atticus@softwarefuze.com>
2021-11-11 07:21:38 -0800

    Create generate command

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+const activate = (context) => {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "helloworld-minimal-sample" is now active!');
@@ -23,7 +23,20 @@ function activate(context) {
         vscode.window.showInformationMessage('Hello World!');
     });
 
+    const generateCommand = vscode.commands.registerCommand('lorem_ipsum.generate', async () => {
+        const res = await vscode.window.showInputBox({
+            placeholder: 'hi'
+        });
+
+        const n = parseInt(res);
+        if (!n) 
+            return vscode.window.showInformationMessage('You must enter a number of words to generate.');
+
+        vscode.window.showInformationMessage('Generating ' + n + ' words...');
+    });
+
     context.subscriptions.push(disposable);
+    context.subscriptions.push(generateCommand);
 };
 
 // this method is called when your extension is deactivated
