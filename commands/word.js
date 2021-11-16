f325bf5007b33e768c66303fb45e0c294efa5297
aroary <meanchemshkedi@gmail.com>
2021-11-16 14:12:40 -0500

    changed to word.js

---
+++ commands/word.js
@@ -0,0 +1,17 @@
+const vscode = require('vscode');
+const generate = require("../utilities/lorem_ipsum");
+
+async function execute() {
+    var count = await vscode.window.showInputBox({ ignoreFocusOut: true, placeHolder: 'Number of words to generate', validateInput });
+    count = parseInt(count);
+    if (!count) return;
+
+    const editor = vscode.window.activeTextEditor;
+    editor.edit(edit => edit.insert(editor.selection.active, generate(count).join` `));
+
+    console.log(`Generated ${count} words`);
+};
+
+const validateInput = value => isNaN(value) ? 'Please enter a number' : null;
+
+module.exports = { name: "word", execute };
