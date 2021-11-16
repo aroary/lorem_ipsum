dc66af54a0ddd2a6693439a7fbccfa9a415a178f
aroary <meanchemshkedi@gmail.com>
2021-11-16 14:13:26 -0500

    generate list

---
+++ commands/list.js
@@ -0,0 +1,18 @@
+const vscode = require('vscode');
+const generate = require("../utilities/lorem_ipsum");
+
+async function execute() {
+    var count = await vscode.window.showInputBox({ ignoreFocusOut: true, placeHolder: 'Number of words to generate', validateInput });
+    count = parseInt(count);
+
+    var text = [];
+    while (text.length < count) text.push(generate(Math.floor(Math.random() * 6) + 16).slice(8).join` ` + ".");
+
+    const editor = vscode.window.activeTextEditor;
+    editor.edit(edit => edit.insert(editor.selection.active, text.join`\n`));
+    console.log(`Generated ${count} list items`);
+};
+
+const validateInput = value => isNaN(value) ? 'Please enter a number' : null;
+
+module.exports = { name: "list", execute };
