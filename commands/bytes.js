b11411ed4fc385c2cd66c0a6c6143aaa90402f0f
aroary <meanchemshkedi@gmail.com>
2021-11-17 19:20:09 -0500

    generate bytes

---
+++ commands/bytes.js
@@ -0,0 +1,25 @@
+const vscode = require('vscode');
+const generate = require("../utilities/lorem_ipsum");
+const random = require("../utilities/randomItem");
+
+async function execute() {
+    var count = await vscode.window.showInputBox({ ignoreFocusOut: true, placeHolder: 'Number of bytes to generate', validateInput });
+    count = parseInt(count);
+
+    var text = [];
+    while (text.join` `.length < count) text.push(generate(9).slice(8)[0]);
+
+    var overflow = text.join` `.length - count;
+    text = text.join` `.slice(0, -overflow);
+
+    if (text[text.length - 1] === " ") text = text.slice(0, -1) + random("abcdefghijklmnopqrstuvwxyz".split``);
+
+    const editor = vscode.window.activeTextEditor;
+    editor.edit(edit => edit.insert(editor.selection.active, text));
+
+    console.log(`Generated ${count} bytes`);
+};
+
+const validateInput = value => isNaN(value) ? 'Please enter a number' : null;
+
+module.exports = { name: "byte", execute };
