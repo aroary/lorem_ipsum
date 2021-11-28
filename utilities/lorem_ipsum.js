255c392dc155ef8b79b1c45bc9d7846d86f87b53
aroary <meanchemshkedi@gmail.com>
2021-11-27 20:11:01 -0500

    dynamic lang gener

--- utilities/lorem_ipsum.js
+++ utilities/lorem_ipsum.js
@@ -1,14 +1,19 @@
 const vscode = require("vscode")
-const words = require('./words.json')
+const fs = require("fs");
+const path = require("path");
+const languages = fs.readdirSync(path.join(__dirname, '../languages')).filter(dir => dir.endsWith('.json')).map(dir => dir.split`.`.shift());
 const randomItem = require('./randomItem');
 
 const generate = (n) => {
-    const language = vscode.workspace.getConfiguration('lorem-ipsum').get('language');
-    const generated = words[language || 'en'].start.split` `;
+    var language = vscode.workspace.getConfiguration('lorem-ipsum').get('language') || 'lat';
+    if (!languages.includes(language)) language = 'lat';
+
+    const languageData = require(`../languages/${language}.json`);
+    const generated = languageData.start.split` `;
 
     if (n > 8) for (let i = 0; i < n - 8; i++) {
-        var word = randomItem(words[language || 'en'].words);
-        while (word === generated[i - 1]) word = randomItem(words);
+        var word = randomItem(languageData.words);
+        while (word === generated[i - 1]) word = randomItem(languageData.words);
 
         generated.push(word);
     } else while (generated[n]) generated.pop();
