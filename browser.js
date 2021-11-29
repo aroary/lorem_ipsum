a710cf932bb1bda113a4e104beaecdcc0877c948
aroary <meanchemshkedi@gmail.com>
2021-11-29 15:52:55 -0500

    browser extension

---
+++ browser.js
@@ -0,0 +1,20 @@
+const vscode = require('vscode');
+
+/**
+ * @description this method is called when your extension is activated
+ * @param {vscode.ExtensionContext} context - The context for the extension
+ */
+function activate(context) {
+    console.log(new Date().toISOString(), 'lorem_ipsum extention activating');
+    console.log(new Date().toISOString(), 'lorem_ipsum extention activated');
+};
+
+/**
+ * @description this method is called when your extension is deactivated
+ */
+function deactivate() {
+    console.log(new Date().toISOString(), "lorem_ipsum extention deactivating");
+    console.log(new Date().toISOString(), "lorem_ipsum extention deactivated");
+};
+
+module.exports = { activate, deactivate };
