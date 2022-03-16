90a12cc99708db83420473af18d418289fa0d959
aroary <meanchemshkedi@gmail.com>
2022-03-15 23:22:16 -0400

    ft

--- index.js
+++ index.js
@@ -12,15 +12,13 @@ const fs = require('fs');
  * @param {vscode.ExtensionContext} context - The context for the extension
  */
 function activate(context) {
-    console.log(new Date().toISOString(), 'lorem_ipsum extention activating');
+    console.log(new Date().toISOString(), 'extention activating');
 
-    fs.readdirSync(__dirname + "/commands", { withFileTypes: true })
-        .filter(file => file.name.split`.`.pop() === "js")
-        .forEach(file => {
-            const command = require(`./commands/${file.name}`);
-            console.log(new Date().toISOString(), `registering command ${command.name} from ${file.name}`);
-            context.subscriptions.push(vscode.commands.registerCommand('lorem_ipsum.' + command.name, command.execute));
-        });
+    fs.readdirSync(__dirname + "/commands").filter(file => file.split`.`.pop() === "js").forEach(file => {
+        const command = require(`./commands/${file}`);
+        console.log(new Date().toISOString(), `registering command ${command.name} from ${file.name}`);
+        context.subscriptions.push(vscode.commands.registerCommand('lorem_ipsum.' + command.name, command.execute));
+    });
 
     console.log(new Date().toISOString(), 'lorem_ipsum extention activated');
 };
