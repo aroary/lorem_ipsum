ec3fa6132817db6049d49995237a6efb66f8b7bc
aroary <meanchemshkedi@gmail.com>
2022-03-29 23:22:42 -0400

    info msg

--- commands/image.js
+++ commands/image.js
@@ -14,17 +14,16 @@ async function execute() {
     var height = await vscode.window.showInputBox({ validateInput: validate, ignoreFocusOut: true, placeHolder: "500", prompt: "height", });
     var lines = await vscode.window.showInputBox({ validateInput: validate, ignoreFocusOut: true, placeHolder: "1", prompt: "lines", });
 
-    if (!width || !height) return;
+    if (!width || !height) return vscode.window.showErrorMessage(`Invalid width or height (${width}x${height})`);
 
     const text = [];
     while (text.length < lines) text.push(generate(Math.floor(Math.random() * 6) + 16).slice(8).join` `);
 
-    const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" overflow="hidden" role="img" aria-label="lorem_ipsum">
-    <title>lorem ipsum</title>
+    const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" overflow="hidden" role="img" aria-label="lorem_ipsum"><title>lorem ipsum</title>
     <linearGradient id="s" x2="0" y2="100%"><stop offset="0%" stop-color="red" stop-opacity=".75"/><stop offset="100%" stop-color="blue" stop-opacity=".75"/></linearGradient>
     <clipPath id="r"><rect width="${width}" height="${height}" rx="10" fill="#fff"/></clipPath><g clip-path="url(#r)"><rect width="${width}" height="${height}" fill="url(#s)"/></g>
     ${text.map((line, index) => `<text x="50%" y="${(100 / (parseInt(lines) + 1)) * (index + 1)}%" fill="#000" dominant-baseline="middle" text-anchor="middle" textLength="80%">${line}</text>`).join``}
-</svg >`;
+    </svg >`;
 
     const editor = vscode.window.activeTextEditor;
     editor.edit(edit => edit.insert(editor.selection.active, "data:image/svg+xml," + encode(svg)));
