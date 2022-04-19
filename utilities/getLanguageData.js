79152ee11679a3db56b6329dc790a24851dda543
aroary <meanchemshkedi@gmail.com>
2022-04-19 09:08:50 -0400

    fix endpoint

--- utilities/getLanguageData.js
+++ utilities/getLanguageData.js
@@ -5,19 +5,19 @@ function getLanguageData(language = null) {
     const config = vscode.workspace.getConfiguration('lorem_ipsum');
 
     if (language) {
-        const endpoint = `https://aroary.github.io/lorem_ipsum/languages/${language}.json`;
+        const endpoint = `https://aroary.com/lorem_ipsum/languages/${language}.json`;
         console.log(new Date().toISOString(), "Getting", endpoint);
         https.request(endpoint, res => {
-            var words = "";
-            res.on('data', data => words += data);
-            res.on("end", () => config.update("language", words, false).then(() => {
+            var data = "";
+            res.on('data', chunk => data += chunk);
+            res.on("end", () => config.update("language", JSON.parse(data), false).then(() => {
                 vscode.window.showInformationMessage(`Language set to ${language}`);
                 console.log(new Date().toISOString(), 'Language updated to', language);
             }));
         }).on('error', error => {
             console.log(new Date().toISOString(), error);
             vscode.window.showErrorMessage(error.message);
-        });
+        }).end();
         return config.get('language');
     } else return config.get('language') || require("./default.json");
 };
