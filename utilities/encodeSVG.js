759b9e49d4fefdcae4628dac0936fc239b10af32
aroary <meanchemshkedi@gmail.com>
2022-07-13 15:50:50 -0400

    better regex for encoding

--- utilities/encodeSVG.js
+++ utilities/encodeSVG.js
@@ -1,10 +1,15 @@
+const e = { '"': "'", "%": "%25", "#": "%23", "{": "%7B", "}": "%7D", "<": "%3C", ">": "%3E" };
+
 /**
  * @description Encode an SVG image to a data URL.
  * @param {string} svg - The SVG string to encode 
  * @returns {string} - The encoded SVG string
  */
 function encodeSvg(svg) {
-    return svg.replace('<svg', ~svg.indexOf('xmlns') ? '<svg' : '<svg xmlns="http://www.w3.org/2000/svg"').replace(/"/g, '\'').replace(/%/g, '%25').replace(/#/g, '%23').replace(/{/g, '%7B').replace(/}/g, '%7D').replace(/</g, '%3C').replace(/>/g, '%3E').replace(/\s+/g, ' ');
+    return svg
+        .replace('<svg', ~svg.indexOf('xmlns') ? '<svg' : '<svg xmlns="http://www.w3.org/2000/svg"')
+        .replace(/["%#{}<>]/g, m => e[m])
+        .replace(/\s+/g, ' ');
 };
 
 module.exports = encodeSvg;
