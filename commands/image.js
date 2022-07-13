588e94a69ac1d662c411db1727d48cb98395186c
aroary <meanchemshkedi@gmail.com>
2022-07-13 16:24:49 -0400

    fix image colors

--- commands/image.js
+++ commands/image.js
@@ -4,25 +4,21 @@ const path = require("path");
 const generate = require("../utilities/lorem_ipsum");
 
 async function execute() {
-    const width = vscode.workspace.getConfiguration('lorem_ipsum').get('image.width') || 500;
-    const height = vscode.workspace.getConfiguration('lorem_ipsum').get('image.height') || 500;
-    const lines = vscode.workspace.getConfiguration('lorem_ipsum').get('image.lines') || 4;
+    const config = vscode.workspace.getConfiguration('lorem_ipsum');
+    const width = config.get('image-width');
+    const height = config.get('image-height');
+    const lines = config.get('image-lines');
     const svg = `
         <svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" overflow="hidden" role="img" aria-label="lorem_ipsum">
             <title>lorem ipsum</title>
-            <clipPath id="r">
-                <rect width="${width}" height="${height}" rx="10" fill="#fff"/>
-            </clipPath>
-            <g clip-path="url(#r)">
-                <rect width="${width}" height="${height}" fill="transparent"/>
-            </g>
+            <rect width="100%" height="100%" fill="${config.get("image-background")}"/>
             ${new Array(lines).fill(undefined).map(() => generate(5).join` `).map((line, index) => `<text x="50%" y="${(100 / (parseInt(lines) + 1)) * (index + 1)}%" fill="#000" dominant-baseline="middle" text-anchor="middle" textLength="80%">
                 ${line}
             </text>`).join``}
         </svg>
     `;
-    const format = vscode.workspace.getConfiguration('lorem_ipsum').get('image') || "png";
-    const file = vscode.Uri.file(path.join(vscode.workspace.workspaceFolders[0].uri.fsPath, vscode.workspace.getConfiguration('lorem_ipsum').get('image.output') || "./", `./${new Date().getTime()}.${format}`));
+    const format = vscode.workspace.getConfiguration('lorem_ipsum').get('image');
+    const file = vscode.Uri.file(path.join(vscode.workspace.workspaceFolders[0].uri.fsPath, config.get('image-output'), `./${new Date().getTime()}.${format}`));
 
     if (vscode.workspace.name) sharp(Buffer.from(svg))[format]().toBuffer().then(buffer => vscode.workspace.fs.writeFile(file, buffer).then(() => {
         console.log(new Date().toISOString(), "Generated", file.path);
